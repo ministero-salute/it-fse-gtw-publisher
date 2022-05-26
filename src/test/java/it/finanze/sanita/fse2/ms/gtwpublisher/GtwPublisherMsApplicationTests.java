0a4ffc2d9dd07363e6b9f133091dbd620991808d
Simone <39314951+simone-lungarella@users.noreply.github.com>
2022-05-26 18:16:52 +0200

    feat(test): improved jUnit test

--- src/test/java/it/finanze/sanita/fse2/ms/gtwpublisher/GtwPublisherMsApplicationTests.java
+++ src/test/java/it/finanze/sanita/fse2/ms/gtwpublisher/GtwPublisherMsApplicationTests.java
@@ -2,8 +2,14 @@ package it.finanze.sanita.fse2.ms.gtwpublisher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import it.finanze.sanita.fse2.ms.gtwpublisher.config.Constants;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ComponentScan(basePackages = {Constants.ComponentScan.BASE})
+@ActiveProfiles(Constants.Profile.DEV)
 class GtwPublisherMsApplicationTests {
 
 	@Test
